47301456690538d90c2b3638222f4630ae3f05f9
franzhf <franzhflores@hotmail.com>
2015-06-28 23:40:12 -0400

    insert editing functionality

--- MeteorTask01/MeteorTask01.js
+++ MeteorTask01/MeteorTask01.js
@@ -8,6 +8,7 @@ if (Meteor.isClient) {
 		}
 	});
 
+	Session.set("oTaskSelected", {});
 	Template.Activity.helpers({
 		activities: function () {
 			return Session.get('ActivitiesCollection');
@@ -15,7 +16,7 @@ if (Meteor.isClient) {
 	});
 
 	Template.Activity.events({
-		'click #add-button': function () {
+		'click #btn-add': function () {
 			// increment the counter when button is clicked
 			var activities = Session.get('ActivitiesCollection');
 			var increment = Session.get('Increment');
@@ -33,7 +34,7 @@ if (Meteor.isClient) {
 	});
 
 	Template.Activity.events({
-			'click button.delete': function(){
+			'click button.btn-delete': function(){
 					var activities = Session.get('ActivitiesCollection');
 					var index = 0;
 					for (var i = activities.length - 1; i >= 0; i--) {						
@@ -52,11 +53,44 @@ if (Meteor.isClient) {
 
 
 	Template.Activity.events({
-			'click .item-task-label': function(){
+			'click .lbl-item-task': function(){
 				this.isEditing = true;
 				console.log(this.isEditing);
-			}
+			},
+			'click .btn-edit': function(e){
+				e.preventDefault();
+        $("#editTaskModal").modal("show");
+        Session.set("oTaskSelected", this);       
+			},
 	}) 
+
+	Template.Task.helpers({
+		taskSelected: function(){
+			console.log('here');
+			return Session.get('oTaskSelected');
+		}
+	})
+
+	Template.Task.events({
+		'click .btn-primary': function(e){
+			this.activityName = $(e).value;
+			var activities = Session.get('ActivitiesCollection');
+			var index = 0;
+				for (var i = activities.length - 1; i >= 0; i--) {						
+					var activity = activities[i];
+					if(this._id === activity.id){
+						index = i;
+						break;
+					}
+				};
+			var act = activities[index];
+			act = this;
+			Session.set('ActivitiesCollection', activities);
+			$("#editTaskModal").modal("hide");
+			console.log(Session.get('ActivitiesCollection'));
+		}
+	});
+
 }
 
 if (Meteor.isServer) {
